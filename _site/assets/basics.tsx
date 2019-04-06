5b9a8b81ed9bb769bf6aa6b85661493c549bb4b2
handachun <chun.handa@gmail.com>
2019-04-06 00:06:07 -0400

    fix _site

--- _site/assets/basics.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="basics" tilewidth="16" tileheight="16" tilecount="120" columns="8">
- <image source="basics.png" width="128" height="240"/>
-</tileset>
