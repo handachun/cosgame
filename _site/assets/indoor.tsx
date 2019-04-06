184455c4aa24dcfdb3f5e82ffad5621530de68f6
handachun <chun.handa@gmail.com>
2019-04-06 00:04:13 -0400

    fixed baseurl

---
+++ _site/assets/indoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="indoor" tilewidth="16" tileheight="16" tilecount="77" columns="7">
+ <image source="indoor.png" width="112" height="191"/>
+</tileset>
